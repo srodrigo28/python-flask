229fd7a1f1cfc7baaaafc4d8762be0bdd9c96255
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2025-12-06 05:10:50 -0300

    pegando dados enviados via post

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 
 # Criando a aplicação Flask
 app = Flask(__name__)
 
-# Criando uma rota
+# Criando uma rota responde tipo GET por padrão
 @app.route('/')
 def root():
     return 'Olá mundo!'
 
-@app.route('/cadastro')
+@app.route('/cadastro', methods=['GET', 'POST'])
 def submit():
+    if request.method == 'POST':
+        data = request.form['nome']
+        return f'Você enviou: {data}'
     return '''
         <form method="POST">
             Nome: <input type="text" name="nome" />
-            Email: <input type="text" name="email" />
             <input type="submit" value="Enviar" />
         </form>
     '''
